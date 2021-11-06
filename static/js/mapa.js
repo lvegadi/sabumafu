1460395d32bbcebd2bf9f7435ba2bb0fa8dc377b
lvegadi <93616988+lvegadi@users.noreply.github.com>
2021-11-05 20:33:41 -0500

    Agregar mapa API

---
+++ static/js/mapa.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibHZlZ2FkaSIsImEiOiJja3ZuNGJqNzJkdWNvMnBtbjVyYjJjdzluIn0.7IQf0OTZQF_NnVcD3HVo1w'
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v11'
+})
