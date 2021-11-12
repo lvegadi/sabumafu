a34e7bf019f1e246a7b13d97f24a252c8c46508b
Damt26 <damttrabajos@gmail.com>
2021-11-12 17:26:03 -0500

    Sabumafu v2.2

--- zonas/views.py
+++ zonas/views.py
@@ -5,12 +5,13 @@ from zonas.models import Fauna, Flora, Zona_protegida, Flora_zona, Fauna_zona
 from zonas.forms import UserRegister
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Permission, User
 import os
-
+from datetime import datetime
 # Create your views here.
 #editar
 def index(request):
-    data = {'title': 'sabumafu'}
+    data = {'title': 'Sabumafu'}
     return render(request, 'zonas/index.html', data)
 #sirve
 def registro(request):
@@ -20,6 +21,9 @@ def registro(request):
             form.save()
             man = form.cleaned_data['username']
             messages.success(request, f'El usuario: {man} ha sido creado.')
+            perm = Permission.objects.get(id=33)
+            user = User.objects.get(username=man)
+            user.user_permissions.add(perm)
     else:
             form = UserRegister()
     data = {'data' : form}
@@ -40,7 +44,7 @@ def zona(request, slug=None):
 def todazona(request):
     lista = Zona_protegida.objects.all()
     count = lista.count()
-    data = {'title': 'zonas protegidas', 'count': count, 'lista': lista}
+    data = {'title': 'Zonas protegidas', 'count': count, 'lista': lista}
     return render(request, 'zonas/todas.html', data)
 #sirve
 def flora(request, id=None):
@@ -79,5 +83,8 @@ def todafauna(request):
 
 @login_required(login_url='/account/login/') #editar
 def alerta(request):
-    data = {'title': 'sabumafu'}
+    zona = Zona_protegida.objects.all()
+    now = datetime.now()
+    date = str(now.day) + " / " + str(now.month) + " / " + str(now.year)
+    data = {'title': 'Alerta', 'zona' : zona, 'date' : date}
     return render(request, 'alerta/index.html', data)
